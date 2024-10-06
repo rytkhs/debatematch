a89d0af0a07608a53bb00bae3323940b86599c00
rytkhs <jag29beem@gmail.com>
2024-10-06 13:21:06 +0900

    refactor: コードの重複を削減

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DebateMatch</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap">
