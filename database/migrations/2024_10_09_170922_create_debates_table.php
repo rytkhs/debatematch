07cc11f063e234d01296be2732a97e2738e65a25
rytkhs <jag29beem@gmail.com>
2025-02-17 00:38:28 +0900

    fix: debatesテーブルの不要なカラムを削除

--- database/migrations/2024_10_09_170922_create_debates_table.php
+++ database/migrations/2024_10_09_170922_create_debates_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
             $table->foreignId('room_id')->constrained()->onDelete('cascade');
             $table->foreignId('affirmative_user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('negative_user_id')->constrained('users')->onDelete('cascade');
-            $table->enum('winner', ['affirmative', 'negative', 'draw'])->nullable();
+            $table->integer('current_turn')->default(0);
+            $table->timestamp('turn_end_time')->nullable();
+            $table->enum('winner', ['affirmative', 'negative'])->nullable();
             $table->timestamps();
         });
     }
