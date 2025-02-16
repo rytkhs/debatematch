9e6c4c155655720a9a6532e56da2426f38cef6a3
rytkhs <jag29beem@gmail.com>
2025-02-17 02:02:40 +0900

    feat: TurnAdvancedイベントをプライベートチャンネルに変更

--- routes/channels.php
+++ routes/channels.php
@@ -7,17 +7,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('debate.{debateId}', function ($user, $debateId) {
-    $debate = Debate::find($debateId);
-    if ($debate && ($debate->affirmative_user_id == $user->id || $debate->negative_user_id == $user->id))
-    {
-        return true;
-    }
-
-    return false;
-
-});
-
 Broadcast::channel('debate.{roomId}', function ($user, $roomId) {
     // ユーザーがそのルームに参加しているか確認
     return $user->rooms()->where('room_id', $roomId)->exists();
