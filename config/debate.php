6a0ab3928ed9a2ecf8dda4b390c9864aecad7474
rytkhs <jag29beem@gmail.com>
2025-02-18 01:53:34 +0900

    feat: ディベートのパート構成を設定ファイルに追加

---
+++ config/debate.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'turns' => [
+        1 => ['name' => '肯定側立論', 'duration' => 362, 'speaker' => 'affirmative', 'is_prep_time' => false],
+        2 => ['name' => '否定側準備時間', 'duration' => 62, 'speaker' => 'negative', 'is_prep_time' => true],
+        3 => ['name' => '否定側質疑', 'duration' => 182, 'speaker' => 'negative', 'is_prep_time' => false],
+        4 => ['name' => '否定側準備時間', 'duration' => 62, 'speaker' => 'negative', 'is_prep_time' => true],
+        5 => ['name' => '否定側立論', 'duration' => 362, 'speaker' => 'negative', 'is_prep_time' => false],
+        6 => ['name' => '肯定側準備時間', 'duration' => 62, 'speaker' => 'affirmative', 'is_prep_time' => true],
+        7 => ['name' => '肯定側質疑', 'duration' => 182, 'speaker' => 'affirmative', 'is_prep_time' => false],
+        8 => ['name' => '否定側準備時間', 'duration' => 62, 'speaker' => 'negative', 'is_prep_time' => true],
+        9 => ['name' => '否定側第1反駁', 'duration' => 242, 'speaker' => 'negative', 'is_prep_time' => false],
+        10 => ['name' => '肯定側準備時間', 'duration' => 122, 'speaker' => 'affirmative', 'is_prep_time' => true],
+        11 => ['name' => '肯定側第1反駁', 'duration' => 242, 'speaker' => 'affirmative', 'is_prep_time' => false],
+        12 => ['name' => '否定側準備時間', 'duration' => 122, 'speaker' => 'negative', 'is_prep_time' => true],
+        13 => ['name' => '否定側第2反駁', 'duration' => 242, 'speaker' => 'negative', 'is_prep_time' => false],
+        14 => ['name' => '肯定側準備時間', 'duration' => 122, 'speaker' => 'affirmative', 'is_prep_time' => true],
+        15 => ['name' => '肯定側第2反駁', 'duration' => 242, 'speaker' => 'affirmative', 'is_prep_time' => false],
+    ],
+];
