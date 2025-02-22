f2f3c7510ff4b4f8e7ef3dd5691ab24e9986833b
rytkhs <jag29beem@gmail.com>
2025-02-23 03:21:07 +0900

    fix: スタイルを修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased" >
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900 flex flex-col">
+        <div class="min-h-screen bg-gray-100 flex flex-col">
             <!-- Page Heading -->
             @isset($header)
             <header>
