b9ba57ca688634c805d0892f647515f6713a0e1e
rytkhs <jag29beem@gmail.com>
2025-03-27 00:42:41 +0900

    feature: DebateFinishedイベントを実装

---
+++ app/Events/DebateFinished.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DebateFinished implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public int $debateId)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('debate.' . $this->debateId),
+        ];
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'message' => 'ディベートが終了しました。AI評価を行っています...',
+            'status' => 'finished'
+        ];
+    }
+}
