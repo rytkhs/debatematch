0fdb54d407031942921b0420ab6f8164e214da2f
rytkhs <jag29beem@gmail.com>
2025-03-26 20:53:16 +0900

    style: レイアウトを強化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
@@ -13,11 +12,12 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined">
+        @stack('styles')
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased" >
-        <div class="min-h-screen bg-gray-100 flex flex-col">
+        <div class="min-h-screen bg-gray-100 flex flex-col @if(!in_array(request()->route()->getName(), ['debate.show', 'rooms.show'])) mt-16 @endif">
             <!-- Page Heading -->
             @isset($header)
             <header>
