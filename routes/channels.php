1bb119704cbaa651f3b4f060530fcfa4da1ad3ad
rytkhs <jag29beem@gmail.com>
2025-03-27 03:42:17 +0900

    refactor: プライベートチャンネルの認証を修正

--- routes/channels.php
+++ routes/channels.php
@@ -7,19 +7,20 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('debate.{roomId}', function ($user, $roomId) {
-    // ユーザーがそのルームに参加しているか確認
-    return $user->rooms()->where('room_id', $roomId)->exists();
+Broadcast::channel('debate.{debateId}', function ($user, $debateId) {
+    $debate = Debate::find($debateId);
+    return $debate && $user->rooms()->where('room_id', $debate->room_id)->exists();
 });
 
 Broadcast::channel('rooms.{roomId}', function ($user, $roomId) {
-    if ($user->rooms()->where('room_id', $roomId)->exists()) {
-        $side = $user->rooms()->where('room_id', $roomId)->first()->pivot->side;
-        return ['id' => $user->id, 'name' => $user->name, 'side' => $side];
-    }
-    return false;
+    return $user->rooms()->where('room_id', $roomId)->exists();
 });
 
 Broadcast::channel('presence-room.{roomId}', function ($user, $roomId) {
     return ['id' => $user->id, 'name' => $user->name];
-    });
+});
+
+
+Broadcast::channel('presence-debate.{debateId}', function ($user, $debateId) {
+    return ['id' => $user->id, 'name' => $user->name];
+});
