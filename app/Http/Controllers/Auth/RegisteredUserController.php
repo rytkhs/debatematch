6ac0db6cd934fdaefde3697dfd329c1805a60412
rytkhs <jag29beem@gmail.com>
2025-03-29 03:40:23 +0900

    feat: メール認証機能を追加

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,6 +45,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('welcome', absolute: false))->with('success', 'アカウントを作成しました');
+        return redirect(route('verification.notice', absolute: false));
     }
 }
