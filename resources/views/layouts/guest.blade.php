776ac340c7a4c08b6975e6c6decf2e1d9714765a
rytkhs <jag29beem@gmail.com>
2025-04-30 22:45:40 +0900

    feat: メタディスクリプションを追加

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="{{ __('messages.app_meta_description') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
